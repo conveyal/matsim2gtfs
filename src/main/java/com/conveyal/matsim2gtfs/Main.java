7db210295b9703592f8d87832bcf6bb03491a360
Andrew Byrd <andrew@fastmail.net>
2016-10-18 15:34:11 +0800

    add main converter class

---
+++ src/main/java/com/conveyal/matsim2gtfs/Main.java
@@ -0,0 +1,142 @@
+package com.conveyal.matsim2gtfs;
+
+import com.conveyal.gtfs.GTFSFeed;
+import com.conveyal.gtfs.model.*;
+import org.mapdb.Fun;
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.geometry.CoordinateTransformation;
+import org.matsim.core.utils.geometry.transformations.TransformationFactory;
+import org.matsim.pt.transitSchedule.api.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDate;
+
+/**
+ * Convert a MatSim XML transit network to GTFS.
+ * There are a few assumptions here that fit only with the FCL MatSim network for Singapore.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Transformation to convert Singapore UTM coordinates to WGS84
+        CoordinateTransformation cT = TransformationFactory.getCoordinateTransformation(TransformationFactory.WGS84_UTM48N, TransformationFactory.WGS84);
+
+        final String FEED_ID = args[0];
+
+        // A dummy scenario into which we will load the XML transit data
+        Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        new TransitScheduleReader(scenario).readFile(args[1]);
+
+        // A GTFS feed into which we will copy the Matsim transit data
+        GTFSFeed gtfsFeed = new GTFSFeed();
+
+        // Add a feed_info to declare the feed ID.
+        FeedInfo gtfsFeedInfo = new FeedInfo();
+        gtfsFeedInfo.feed_id = FEED_ID;
+        gtfsFeedInfo.feed_lang = "en_us";
+        gtfsFeedInfo.feed_publisher_name = "Conveyal, LLC";
+        gtfsFeedInfo.feed_publisher_url = createUrl("http://www.conveyal.com");
+        gtfsFeed.feedInfo.put("info", gtfsFeedInfo);
+
+        // Create a single agency in this new GTFS feed.
+        Agency gtfsAgency = new Agency();
+        gtfsAgency.agency_id = FEED_ID;
+        gtfsAgency.agency_name = FEED_ID;
+        gtfsAgency.agency_timezone = "America/New_York";
+        gtfsAgency.agency_url = createUrl("http://www.conveyal.com");
+        gtfsFeed.agency.put(gtfsAgency.agency_id, gtfsAgency);
+
+        // A Matsim network describes only a single day of operation.
+        // Create a single GTFS service called S.
+        CalendarDate gtfsCalendarDate = new CalendarDate();
+        gtfsCalendarDate.date = LocalDate.of(2017, 1, 1);
+        gtfsCalendarDate.exception_type = 1; // Add service on this date.
+        gtfsCalendarDate.service_id = "S";
+        Service gtfsService = new Service(gtfsCalendarDate.service_id);
+        gtfsService.calendar_dates.put(gtfsCalendarDate.date, gtfsCalendarDate);
+        gtfsFeed.services.put(gtfsService.service_id, gtfsService);
+
+        // Copy all stops from Matsim scenario to GTFS
+        for (TransitStopFacility matsimStop : scenario.getTransitSchedule().getFacilities().values()) {
+            Stop gtfsStop = new Stop();
+            gtfsStop.stop_id = matsimStop.getId().toString();
+            gtfsStop.stop_name = matsimStop.getName();
+            Coord wgsCoord = cT.transform(matsimStop.getCoord());
+            gtfsStop.stop_lat = wgsCoord.getY();
+            gtfsStop.stop_lon = wgsCoord.getX();
+            gtfsFeed.stops.put(gtfsStop.stop_id, gtfsStop);
+        }
+
+        // Next convert all Matsim TransitLines to GTFS Routes
+        for (TransitLine matsimLine : scenario.getTransitSchedule().getTransitLines().values()) {
+
+            Route gtfsRoute = new Route();
+            gtfsRoute.feed_id = FEED_ID;
+            gtfsRoute.agency_id = FEED_ID;
+            gtfsRoute.route_id = matsimLine.getId().toString();
+            gtfsRoute.route_short_name = matsimLine.getId().toString();
+            gtfsRoute.route_long_name = matsimLine.getName();
+            // Routes with IDs starting with a letter are train lines. Numbers are bus lines.
+            gtfsRoute.route_type = Character.isLetter(gtfsRoute.route_id.charAt(0)) ? 1 : 3;
+            gtfsFeed.routes.put(gtfsRoute.route_id, gtfsRoute);
+
+            // Within a Matsim TransitLine, convert each Matsim TransitRoute to what Conveyal calls a "trip pattern"
+            // (a bunch of GTFS trips with the same stop sequence).
+            // Note that this explodes MatSim's representation (which in GTFS would use frequencies.txt with
+            // exactTimes = true) into a bunch of separate trips all with the same inter-stop times.
+            for (TransitRoute matsimRoute : matsimLine.getRoutes().values()) {
+                // matsimRoute.getTransportMode();
+                String gtfsPatternId = matsimRoute.getId().toString();
+                int tripOnPattern = 0;
+                for (Departure matsimDeparture : matsimRoute.getDepartures().values()) {
+
+                    Trip gtfsTrip = new Trip();
+                    gtfsTrip.route_id = gtfsRoute.route_id;
+                    gtfsTrip.service_id = "S"; // All trips are on the same service day.
+                    gtfsTrip.trip_id = gtfsPatternId + "_" + tripOnPattern;
+                    gtfsTrip.trip_headsign = matsimRoute.getStops().get(matsimRoute.getStops().size() - 1).getStopFacility().getName();
+                    gtfsTrip.block_id = matsimDeparture.getVehicleId().toString();
+                    // gtfsTrip.trip_short_name;
+                    gtfsFeed.trips.put(gtfsTrip.trip_id, gtfsTrip);
+
+                    // The first departure time on a Matsim trip is in seconds after midnight.
+                    // The Matsim arrival and departure offsets are also in seconds.
+                    double firstDepartureTime = matsimDeparture.getDepartureTime();
+                    int stopWithinTrip = 0;
+                    for (TransitRouteStop trs : matsimRoute.getStops()) {
+                        StopTime gtfsStopTime = new StopTime();
+                        gtfsStopTime.trip_id = gtfsTrip.trip_id;
+                        gtfsStopTime.stop_sequence = stopWithinTrip;
+                        gtfsStopTime.stop_id = trs.getStopFacility().getId().toString();
+                        gtfsStopTime.arrival_time = (int) (firstDepartureTime + trs.getArrivalOffset());
+                        gtfsStopTime.departure_time = (int) (firstDepartureTime + trs.getDepartureOffset());
+                        gtfsFeed.stop_times.put(
+                                new Fun.Tuple2(gtfsStopTime.trip_id, Integer.valueOf(gtfsStopTime.stop_sequence)),
+                                gtfsStopTime);
+                        stopWithinTrip += 1;
+                    }
+                    tripOnPattern += 1;
+                }
+            }
+        }
+        gtfsFeed.toFile(args[2]);
+
+    }
+
+    /**
+     * Circumvent Java "safety".
+     */
+    private static URL createUrl(String url) {
+        try {
+            return new URL(url);
+        } catch (MalformedURLException error) {
+            throw new IllegalArgumentException(error.getMessage(), error);
+        }
+    }
+
+}
